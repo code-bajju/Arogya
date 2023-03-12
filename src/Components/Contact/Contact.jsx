09e2b20c91bd3064ddb104817df8701d4c0e54f4
code-bajju <11212766@mmec.mmdu.org>
2023-03-12 15:03:29 +0530

    Contact Form Added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -24,14 +24,14 @@ const ContactForm = () => {
     <section className="text-gray-400 bg-gray-100 body-font">
       <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
         <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-          <h1 className="title-font font-medium text-3xl text-gray-900">Slow-carb next level shoindxgoitch ethical authentic, poko scenester</h1>
-          <p className="leading-relaxed mt-4 text-gray-700">Poke slow-carb mixtape knausgaard, typewriter street art gentrify hammock starladder roathse. Craies vegan tousled etsy austin.</p>
+          <h1 className="title-font font-medium text-3xl text-[#54284c]">Contact Us</h1>
+          <p className="leading-relaxed mt-4 text-[#54284c]">Feel Free to contact us for any query.</p>
         </div>
         <div className="lg:w-2/6 md:w-1/2 bg-white bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-          <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
+          
           <form onSubmit={handleSubmit}>
-            <div className="relative mb-4">
-              <label htmlFor="full-name" className="leading-7 text-sm text-gray-600">Full Name</label>
+          <div className="relative mb-4">
+              <label htmlFor="full-name" className="leading-7 text-sm text-[#54284c]">Full Name</label>
               <input
                 type="text"
                 id="full-name"
@@ -42,7 +42,7 @@ const ContactForm = () => {
               />
             </div>
             <div className="relative mb-4">
-              <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+              <label htmlFor="email" className="leading-7 text-sm text-[#54284c]">Email</label>
               <input
                 type="email"
                 id="email"
@@ -52,9 +52,14 @@ const ContactForm = () => {
                 className="w-full bg-gray-300 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-purple-900 rounded border border-gray-400 focus:border-purple-500 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
-            <button type="submit" className="text-gray-900 bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">Button</button>
+            <div className="relative mb-4">
+              <label htmlFor="full-name" className="leading-7 text-sm text-[#54284c]">Message</label>
+             
+    
+            <textarea  className="w-full bg-gray-300 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-purple-900 rounded border border-gray-400 focus:border-purple-500 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" name="comment" form="usrform"></textarea>
+            </div>
+            <button type="submit" className="text-gray-300 bg-[#54284c] border-0 py-1 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">Submit</button>
           </form>
-          <p className="text-sm mt-3 text-gray-700">Literally you probably haven't heard of them jean shorts.</p>
         </div>
       </div>
     </section>
