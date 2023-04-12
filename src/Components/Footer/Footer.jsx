b3f58c067e021fa95e1ac903b1bb8a0182e3bd42
code-bajju <11212766@mmec.mmdu.org>
2023-04-12 15:04:09 +0530

    Footer Added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="flex items-center justify-center p-4 bg-base-300 text-base-content">
     <aside>
-      <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+      <p>Copyright © 2024 - All right reserved by CyberBoys</p>
     </aside>
   </footer>
   );
