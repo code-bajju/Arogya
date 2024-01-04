ba63fe247e762363718011aaa3062693489f774f
code-bajju <11212766@mmec.mmdu.org>
2024-01-04 11:31:39 +0530

    Navbar Hamburger

--- src/Components/Navbar/nav.js
+++ src/Components/Navbar/nav.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import logo from "./logo.png";
+import './Navbar.css'; // Import your CSS file
 
 const Navbar = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -73,23 +74,33 @@ const Navbar = () => {
           </div>
           <div>
        
-            
-         <ul className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+              <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Home</a>
+              </li>
+              <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
           <li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Home</a></li>
           <li className="text-gray-300">
-         
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            </svg>
           </li>
           <li><a className="text-sm text-blue-600 font-bold" href="#">About Us</a></li>
           <li className="text-gray-300">
-           
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            </svg>
           </li>
           <li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Services</a></li>
           <li className="text-gray-300">
-           
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            </svg>
           </li>
           <li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Pricing</a></li>
           <li className="text-gray-300">
-           
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            </svg>
           </li>
           <li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Contact</a></li>
     
