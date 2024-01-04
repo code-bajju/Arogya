c38b996b39129d8f804484cb36aee515cb65dee9
code-bajju <11212766@mmec.mmdu.org>
2024-01-04 11:42:14 +0530

    Updated Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,13 +23,13 @@ const Navbar = () => {
           </button>
         </div>
         <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
-          <li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Home</a></li>
+          <li><a className="text-sm text-blue-600 font-bold hover:text-gray-500" href="#">Home</a></li>
           <li className="text-gray-300">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
             </svg>
           </li>
-          <li><a className="text-sm text-blue-600 font-bold" href="#">About Us</a></li>
+          <li><a className="text-sm text-gray-400" href="#">About Us</a></li>
           <li className="text-gray-300">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
@@ -75,11 +75,11 @@ const Navbar = () => {
        
             
          <ul className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
-          <li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Home</a></li>
+          <li><a className="text-sm text-blue-600 font-bold hover:text-gray-500" href="#">Home</a></li>
           <li className="text-gray-300">
          
           </li>
-          <li><a className="text-sm text-blue-600 font-bold" href="#">About Us</a></li>
+          <li><a className="text-sm text-gray-400" href="#">About Us</a></li>
           <li className="text-gray-300">
            
           </li>
